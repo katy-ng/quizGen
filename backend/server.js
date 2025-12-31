7b4a42bc25a774ebc75cd53be2e341514ed32690
Katy Ng <katyng3000@gmail.com>
2025-12-31 14:53:27 -0500

    created the files

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+/*NOT linked to an HTML, so everything is private (can't be seen in browser DevTools)
+Purpose:
+  1. Import OpenAI
+  2. Send extracted pdf text as a prompt
+*/
---
+++ frontend/quiz.js
@@ -0,0 +1,6 @@
+/*linked to HTML file so this file is PUBLIC in browser dev tools
+Purpose:
+  1. Capture file input, send PDF to backend using fetch() and FormData
+  2. Display each question and answer choices of the quiz
+  3. Add functionality to buttons, etc.
+*/
